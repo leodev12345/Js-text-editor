44b4bec8f8d3e6adf6c6ed8e6fe5fce50e4dfcc7
LeoDev <leonardzagar@gmail.com>
2019-04-19 18:17:44 +0200

    Second commit

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
 function edit(){
 	editor.document.designMode = "On";
 }
-function bold(){
-	editor.document.execCommand("bold", false, null);
-}
-function italic(){
-	editor.document.execCommand("italic", false, null);
-}
-function underline(){
-	editor.document.execCommand("underline", false, null);
-}
-function subscript(){
-	editor.document.execCommand("subscript", false, null);
-}
-function superscript(){
-	editor.document.execCommand("superscript", false, null);
-}
-function undo(){
-	editor.document.execCommand("undo");
-}
-function redo(){
-	editor.document.execCommand("redo");
+function cmd(cmd){
+	editor.document.execCommand(cmd, false, null);
 }
