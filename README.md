d7352301f06bc71367e9cb95b3812a9e1cfbde85
LeoDev <leonardzagar@gmail.com>
2019-04-21 19:04:41 +0200

    Third commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## This is simple WYSIWYG editor
+### Update 2:
+- [x] You can now change text color and font size
+- [x] New icon for select all and add hr
+- [x] Adden New document button
--- main.js
+++ main.js
@@ -4,3 +4,12 @@ function edit(){
 function cmd(cmd){
 	editor.document.execCommand(cmd, false, null);
 }
+function cmdWithArg(command, arg){
+	editor.document.execCommand(command, false, arg);
+}
+function newFile(){
+	let c = confirm("The changes cannot be saved,are you sure to continue?")
+	if(c==true){
+		location.reload();
+	}
+}
