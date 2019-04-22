00c6859f702f0106a6609a42f379abee0a61d61a
LeoDev <leonardzagar@gmail.com>
2019-04-22 15:20:08 +0200

    Fourth commit

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ## This is simple WYSIWYG editor
-### Update 2:
-- [x] You can now change text color and font size
-- [x] New icon for select all and add hr
-- [x] Adden New document button
+### Update 3:
+- [x] Added insert paragraph
+- [x] Added align justify
+- [x] Added insert image
+- [x] Added link and unlink text option
+- [x] Added exit and enter edit mode button
+- [x] You can now hightlight text
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let isInEditMode = true;
 function edit(){
 	editor.document.designMode = "On";
 }
@@ -11,5 +12,23 @@ function newFile(){
 	let c = confirm("The changes cannot be saved,are you sure to continue?")
 	if(c==true){
 		location.reload();
+	}
+}
+function link(){
+	let link = prompt("Enter URL:");
+	editor.document.execCommand("createLink",false,link);
+}
+function image(){
+	let img = prompt("Enter image URL:");
+	editor.document.execCommand("insertImage",false,img);
+}
+function editMode(){
+	if(isInEditMode){
+		editor.document.designMode = "Off";
+		isInEditMode = false;
+	}
+	else{
+		editor.document.designMode = "On";
+		isInEditMode = true;
 	}
 }
